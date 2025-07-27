8b2ab51f7494ffca9093bae8e8cdd310625094fe
Johan Henriksson <mahogny@areta.org>
2025-07-27 21:05:31 +0200

    more logs

--- server/src/main.rs
+++ server/src/main.rs
@@ -50,8 +50,8 @@ struct ConfigFile {
 /// Backend entry point
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    //std::env::set_var("RUST_LOG", "info");
-    //std::env::set_var("RUST_BACKTRACE", "1");
+    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
     let config_file:ConfigFile = serde_json::from_reader(
