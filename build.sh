2ff37108ed097882a1e931f058629552aaa0994a
Johan Henriksson <mahogny@areta.org>
2025-07-28 16:35:19 +0200

    build fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-cd app; cargo build
 
-cd src; cargo build
+
+cargo install cargo-watch
+cargo install trunk
+rustup target add wasm32-unknown-unknown
+
+cd app; trunk watch #cargo build
+
+#cd src; cargo build
+
+cargo watch -w server -w src -x "run"
+
 
 #unsure of order. app first?
 
